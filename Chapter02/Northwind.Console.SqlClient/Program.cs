a9ba1b54340c04439e40c2ad934bc7270ce972e4
Luis Gerardo Calderon Contreras <luisg.calderon@devcalcon.com>
2025-03-29 14:25:16 -0500

    Add SQL connection

---
+++ Chapter02/Northwind.Console.SqlClient/Program.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Data.SqlClient; //To use SqlConnection and so on.
+ConfigureConsole();
+#region Set up the connection string builder
+SqlConnectionStringBuilder builder = new()
+{
+    InitialCatalog = "Northwind",
+    MultipleActiveResultSets = true,
+    Encrypt = true,
+    TrustServerCertificate = true,
+    ConnectTimeout = 10 // Default is 30 secs
+};
+WriteLine("Connect to:");
+WriteLine(" 1 - SQL Server on local machine");
+WriteLine(" 2 - Azure SQL Database");
+WriteLine(" 3 - Azure SQL Edge");
+WriteLine();
+Write("Press a key: ");
+ConsoleKey key = ReadKey().Key;
+WriteLine(); WriteLine();
+switch (key)
+{
+    case ConsoleKey.D1 or ConsoleKey.NumPad1:
+        builder.DataSource = ".";
+        break;
+    case ConsoleKey.D2 or ConsoleKey.NumPad2:
+        builder.DataSource =
+            // User your Azure SQL Databas server name.
+            "tcp:apps-services-book-lgcc.database.windows.net,1433";
+        break;
+    case ConsoleKey.D3 or ConsoleKey.NumPad3:
+        builder.DataSource = "tcp:127.0.0.1,1433";
+        break;
+    default:
+        WriteLine("No data source selected.");
+        return;
+}
+WriteLine("Authenticate using:");
+WriteLine(" 1 - Windows Ingrated Security");
+WriteLine(" 2 - SQL Login, for example, sa");
+WriteLine();
+Write("Press a key: ");
+key = ReadKey().Key;
+WriteLine(); WriteLine();
+if (key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+{
+    builder.IntegratedSecurity = true;
+}
+else if (key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+{
+    Write("Enter your SQL server user ID: ");
+    string? userId = ReadLine();
+    if (string.IsNullOrWhiteSpace(userId))
+    {
+        WriteLine("User Id cannot be empty or null");
+        return;
+    }
+    builder.UserID = userId;
+    Write("Enter your SQL Server password: ");
+    string? password = ReadLine();
+    if (string.IsNullOrWhiteSpace(password))
+    {
+        WriteLine("Password cannot be empty or null");
+        return;
+    }
+    builder.Password = password;
+    builder.PersistSecurityInfo = false;
+}
+else
+{
+    WriteLine("No authentication selected.");
+    return;
+}
+
+#endregion
+#region Create and optn the connection
+SqlConnection connection = new(builder.ConnectionString);
+WriteLine(connection.ConnectionString);
+WriteLine();
+connection.StateChange += Connection_StateChange;
+connection.InfoMessage += Connection_InfoMessage;
+try
+{
+    WriteLine("Openning connection. Please wait up to {0} seconds...",
+        builder.ConnectTimeout);
+    WriteLine();
+    connection.Open();
+    WriteLine($"SQL Server version: {connection.ServerVersion}");
+}
+catch (SqlException ex)
+{
+    WriteLineInColor($"SQL exception: {ex.Message}",
+        ConsoleColor.Red);
+    return;
+}
+#endregion
+connection.Close();
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # appsnet9
+
+Main code repository
+https://github.com/markjprice/apps-services-net8/
+
+Code to add in csproj
+<TreatWarningsAsErrors>true</TreatWarningsAsErrors>
